4b426e9efa6cc5db7b6c7881750d94064ced1ff4
Egle Tilenyte <egle.tilenyte@gmail.com>
2025-01-04 21:10:42 +0200

    6th

--- report_sending.py
+++ report_sending.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import Main_file
+import smtplib
+from email.mime.text import MIMEText
 
 def main():
     which_report_to_send()
@@ -7,13 +10,41 @@ def main():
 def which_report_to_send():
     report_list = get_report_list()
     if not report_list:
-        print("You currently have no reports saved. Try recording a period as done and then you can receive it via email.")
+        print("You currently have no reports saved. Try recording a period as complete and then you can receive it via email.")
+        Main_file.main()
     else:
         print("Your current reports are:")
         for report in report_list:
-            print(report)   #.remove(".txt")
+            print(report.replace(".txt", ""))   
         print("Which one would you like to receive via email?")
+        report = get_user_input(report_list)
+        read_report(report)
+        report_body = read_report(report)
+        body = (f"Hello!\n\nHere is your report:\n\n{report_body}")
+        subject = "Financial report"
+        sender = "egletestingpython@gmail.com"
+        recipients = "egletestingpython@gmail.com"
+        password = "ixed nqiz qwqk prfc"
+        send_email(subject, body, sender, recipients, password)
 
+
+def get_user_input(list):
+    while True:
+        user_selected_report = input("Report: ").strip().lower()
+        user_selected_report = user_selected_report +".txt"
+        if user_selected_report not in list:
+            print("This report does not exist")
+            continue
+        else:
+            return user_selected_report 
+
+
+def read_report(report):
+    body = []
+    with open(report, "r") as file:
+        lines = file.readlines()    
+        body.append(lines)
+    return body
 
 
 def get_report_list():
@@ -26,6 +57,16 @@ def get_report_list():
         txt_files.remove("README.txt") 
     return txt_files
 
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipients
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
 
 if __name__ == "__main__":
     main()
